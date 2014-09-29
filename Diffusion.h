4e098f4f715503c81e1ae0d9d104758ab1c5c644
shelleysu84 <shelleysu84@gmail.com>
2014-09-28 20:04:33 -0400

    Update Diffusion.h

--- Diffusion.h
+++ Diffusion.h
@@ -1,2 +1,16 @@
 #include<iostream>
+
 using namespace std;
+
+typedef double Rate;
+typedef double Time;
+
+class DiffusionProcess{
+	double x0_; //init value
+public:
+	DiffusionProcess(double x0): x0_(x0) {
+		
+	}	
+	~DiffusionProcess(){}
+  
+}
