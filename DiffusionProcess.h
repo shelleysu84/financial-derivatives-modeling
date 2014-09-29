9db2a79c0669145a6e5ecda47bb986d5eb06188b
shelleysu84 <shelleysu84@gmail.com>
2014-09-28 20:21:41 -0400

    Update DiffusionProcess.h

--- DiffusionProcess.h
+++ DiffusionProcess.h
@@ -1,3 +1,6 @@
+/*
+General Diffusion process : dX = drift * dT + sigma * dZ 
+*/
 #include<iostream>
 
 using namespace std;
@@ -12,20 +15,21 @@ public:
 	DiffusionProcess(double x0): x0_(x0) {}	
 	virtual ~DiffusionProcess(){}
 	
-	double x0() const{	
+	double getValue() const{	
 		return x0_;	
 	}
-	
+	//define interface for drift part and stochastic process part
 	virtual double drift(Time t, Value x) const = 0;
 	virtual double diffusion(Time t, Value x) const = 0;
 	
+	//Ex = x0_ + drift value
 	virtual double expectation(Time t0, Value x0, Time dt) const{
-		return x0+drift(t0,x0)*dt;
+		return x0 + drift(t0,x0)*dt;
 	}
 	
+	//Var x = sigma^2 * dt for diffusion process
 	virtual double variance(Time t0, Value x0, Time dt) const{
 		double sigma = diffusion(t0,x0);
 		return sigma*sigma*dt;
 	}
-  
-}
+};
