694a68b6a235ade6a0e3f3e11ca3ed77e0bb28ee
shelleysu84 <shelleysu84@gmail.com>
2014-09-28 20:10:32 -0400

    Update Diffusion.h

--- Diffusion.h
+++ Diffusion.h
@@ -4,13 +4,22 @@ using namespace std;
 
 typedef double Rate;
 typedef double Time;
+typedef double Value;
 
 class DiffusionProcess{
 	double x0_; //init value
 public:
-	DiffusionProcess(double x0): x0_(x0) {
-		
-	}	
-	~DiffusionProcess(){}
+	DiffusionProcess(double x0): x0_(x0) {}	
+	virtual ~DiffusionProcess(){}
+	
+	double x0() const{	return x0_;	}
+	
+	virtual double drift(Time t, Value x) const = 0;
+	virtual double diffusion(Time t, Value x) const = 0;
+	
+	virtual double expectation(Time t0, Value x0, Time dt) const{
+		return x0+drift(t0,x0)*dt;
+	}
+	
   
 }
