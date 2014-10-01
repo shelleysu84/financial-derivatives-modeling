6a2717d816f2d97ffb87f5340f14b260a16a88d0
shelleysu84 <shelleysu84@gmail.com>
2014-10-01 16:12:36 -0400

    Update Instrument.h

--- Instrument.h
+++ Instrument.h
@@ -4,15 +4,29 @@ using namespace std;
 class Instrument : public Patterns::Observer, public Patterns::Observable {
 
 	std::string isinCode_ , descrition_ ;
-	mutable bool calculated;
+	mutable bool calculated_;
 	
 protected:
-	mutable bool isExpired;
-	mutable double NPV;
+	mutable bool isExpired_;
+	mutable double NPV_;
 	
 	virtual void performCalculations() const = 0;
 public:
+	Instrument (const std::string& isinCode, const std::string& description) : NPV_(0.0), isExpired(false), calculated(false) {
 	
+	}
+	
+	inline std::string isinCode() const{
+		return isinCode_;
+	}
+	
+	inline double NPV() const{
+		calculate();
+		return isExpired_? 0.0 : NPV_;
+	}
+	inline std::string description() const{
+		return descrition_ ;
+	}
 	
 }
 
